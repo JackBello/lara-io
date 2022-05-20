3873c3fb242cb03b3101cddab69782f0e9114d39
lufer96 <lufer96>
2022-05-19 20:11:33 -0500

    👌 IMPROVE: deps

--- lib/dep.ts
+++ lib/dep.ts
@@ -1,7 +1,7 @@
 import { Reflect as _reflect } from "https://deno.land/x/deno_reflect@v0.1.13/mod.ts";
 import { NIL_UUID, V4, isNil, isValid, uuid } from "https://deno.land/x/uuid@v0.1.2/mod.ts";
 import { delay } from "https://deno.land/std@0.129.0/async/mod.ts";
-import { fromFileUrl, dirname, resolve, toFileUrl, join, extname } from "https://deno.land/std@0.110.0/path/mod.ts";
+import { fromFileUrl, dirname, resolve, toFileUrl, join, extname, parse, basename } from "https://deno.land/std@0.110.0/path/mod.ts";
 import { readableStreamFromReader } from "https://deno.land/std@0.134.0/streams/mod.ts";
 import { ensureSymlinkSync } from "https://deno.land/std@0.136.0/fs/mod.ts";
 
@@ -17,7 +17,9 @@ export const Path = {
     fromFileUrl,
     toFileUrl,
     join,
-    extname
+    extname,
+    parse,  
+    basename,
 }
 export const Reflect = _reflect;
 export const UUID = {
