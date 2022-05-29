b837559cb36741ca7b050568a5493c380c53a933
JackBello <vitrollers@outlook.com>
2022-05-28 23:31:44 -0500

    📦 NEW: tipos e interfaces para las clases y funciones del framework

---
+++ framework/@types/index.ts
@@ -0,0 +1,6 @@
+// deno-lint-ignore-file ban-types
+export type TSettingRegisterSevice = {
+    isCallback: boolean,
+    isSingleton: boolean,
+    configService: object
+}
---
+++ framework/@types/route.ts
@@ -0,0 +1,64 @@
+// deno-lint-ignore-file no-explicit-any
+import { TMethodHTTP } from './server.ts';
+import { TRouteContext } from '../modules/types.ts';
+
+// interfaces
+
+export interface IRouterFile {
+    prefix?: string;
+    subdomain?: string;
+    path: string;
+}
+
+export interface IGroupConfig extends IGroup {
+    routes: Array<IRouteConfig>;
+}
+
+export interface IRouteConfig extends IRoute {
+    group?: IGroupConfig
+}
+
+export interface IGroup {
+    [key: string]: any[];
+}
+
+export interface IRoute {
+    uri: string,
+    name?: string,
+    method?: TMethodHTTP | Array<TMethodHTTP>;
+    middleware?: Array<any> | any;
+    regexp?: string | string[];
+    dependencies?: Array<string>;
+    redirect?: boolean;
+    domain?: string;
+    action: any;
+}
+
+export interface IHistoryRoute {
+    uri: string,
+    name?: string,
+    meta?: any;
+}
+
+export interface ISettingRoute {
+    uri?: string;
+    method?: TMethodHTTP | Array<TMethodHTTP>;
+    middleware?: any | Array<any>;
+    regexp?: string | string[];
+    name?: string;
+    redirect?: boolean;
+}
+
+export interface IHistory {
+    previousRoutes: Array<IHistoryRoute>;
+    currentRoute: Array<IHistoryRoute>;
+    nextRoutes: Array<IHistoryRoute>;
+}
+
+export type TCallbackRoute = (context: TRouteContext, ...params: any) => any;
+
+export type TCallbackMiddleware = (context: TRouteContext, next: () => any) => any;
+
+export type TAction = string | any[] | TCallbackRoute;
+
+export type TMiddleware = string | any[] | TCallbackMiddleware | TCallbackMiddleware[];
