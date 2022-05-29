cfa80cb28309196375e0358947d4761256432787
JackBello <vitrollers@outlook.com>
2022-05-28 23:36:41 -0500

    📦 NEW: LaraIO framework import modules

---
+++ framework/modules/fecades.ts
@@ -0,0 +1,5 @@
+export * from '../fecades/app.fecade.ts';
+export * from '../fecades/route.fecade.ts';
+export * from '../fecades/request.fecade.ts';
+export * from '../fecades/history.fecade.ts';
+export * from '../fecades/template.fecade.ts';
---
+++ framework/modules/providers.ts
@@ -0,0 +1,5 @@
+export * from '../providers/fundation.provider.ts';
+export * from '../providers/template.provider.ts';
+export * from '../providers/router.provider.ts';
+export * from '../providers/server.provider.ts';
+export * from '../providers/http.provider.ts';
